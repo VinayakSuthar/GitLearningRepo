b235049419eb0c80cefb69c084fa23f405655b2f
Vinayak <sutharvinayak28@gmail.com>
2020-08-25 21:12:24 +0530

    while loop understood

---
+++ while.js
@@ -0,0 +1,14 @@
+let game='pbg';
+switch(game) {
+    case "cod":
+        console.log('Call of duty');
+        break;
+    case "coc":
+        console.log('Clash of clans');
+        break;
+    case "pubg":
+        console.log('Players Unkonwn Battle Ground');
+        break;
+    default:
+        console.log('Any other');
+};
